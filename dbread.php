d991c12f445e9a9198989ff6e73ec546b0356da3
CanEngineer <nathandykstra@Nathans-MBP.lan>
2018-11-25 08:38:44 -0500

    perm's

--- dbread.php
+++ dbread.php
@@ -1,1 +1,29 @@
+<?php
+phpinfo();
 
+
+$servername = "localhost";
+$username = "pi";
+$password = "raspberry";
+$dbname = "temper";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT temp,datetime,location FROM temper";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "temp: " . $row["temp"]. " - date: " . $row["datetime"]. " location " . $row["location"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
